e5620c809c76108cc420277ec7db86e5dde7636c
Brendan Loukes <02brendan.loukes@gulllakecs.org>
2020-01-23 08:27:36 -0500

    Add Double For Loop
    We created an outer for loop i, and then nested an inner for loop k

---
+++ ControlFlow.py
@@ -0,0 +1,9 @@
+
+# Programmer: Brendan Loukes
+# Date 1.20.20
+# Program: Double For Loop
+
+for i in range(3):
+    print("outer for loop " + str(i))
+    for k in range(4):
+        print("\tinner for loop " + str(k))
